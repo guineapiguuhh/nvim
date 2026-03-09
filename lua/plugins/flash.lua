a5677f9028f8a6e41ad3c2bd3e7e6d1ed9bee36f
GuineaPigUuhh <pedromaxaretz@gmail.com>
2026-03-08 21:39:22 -0300

    add flash!

---
+++ lua/plugins/flash.lua
@@ -0,0 +1,17 @@
+---@type LazyPluginSpec
+return {
+	"folke/flash.nvim",
+	event = "VeryLazy",
+	---@type Flash.Config
+	opts = {},
+	keys = {
+		{
+			"S",
+			mode = { "n", "x", "o" },
+			function()
+				require("flash").jump()
+			end,
+			desc = "Flash",
+		},
+	},
+}
