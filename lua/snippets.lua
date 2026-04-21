4ed4dac07c7e0255c4ca5efa9877e151be53cdf7
GuineaPigUuhh <pedromaxaretz@gmail.com>
2026-04-21 00:11:15 -0300

    more snippets and rework

--- lua/snippets.lua
+++ lua/snippets.lua
@@ -3,56 +3,66 @@ local s = LuaSnip.snippet
 local text = LuaSnip.text_node
 local insert = LuaSnip.insert_node
 
+LuaSnip.add_snippets("odin", {
+    s("procedure", {
+        insert(1),
+        text({ " :: proc(" }),
+        insert(2),
+        text({ ") {", "}" }),
+    }),
+
+    s("function", {
+        insert(1),
+        text({ " :: proc(" }),
+        insert(2),
+        text({ ") -> " }),
+        insert(3),
+        text({ " {", "}" }),
+    }),
+
+    s("struct", {
+        insert(1),
+        text({ " :: struct {", "}" }),
+    }),
+
+    s("union", {
+        insert(1),
+        text({ " :: union {", "}" }),
+    }),
+
+    s("enum", {
+        insert(1),
+        text({ " :: enum {", "}" }),
+    }),
+})
+
 LuaSnip.add_snippets("go", {
-	s("struct", {
+    s("function", {
+        text({ "func " }),
+        insert(1),
+        text({ "(" }),
+        insert(2),
+        text({ ") " }),
+        insert(3),
+        text({ " {", "}" }),
+    }),
+
+    s("struct", {
 		text({ "type " }),
-		insert(1, "Name"),
-		text({ " struct {" }),
-		insert(2),
-		text({ "}" }),
+		insert(1),
+		text({ " struct {", "}" }),
 	}),
+
 	s("interface", {
 		text({ "type " }),
-		insert(1, "Name"),
-		text({ " interface {" }),
-		insert(2),
-		text({ "}" }),
+		insert(1),
+		text({ " interface {", "}" }),
 	}),
+
 	s("enum", {
 		text({ "type " }),
-		insert(1, "Name"),
-		text({ " uint" }),
-		text({ "", "", "" }),
-		text({ "const (" }),
-		insert(2),
-		text({ ")" }),
-	}),
-
-	s("main", {
-		text({ "func main() {" }),
 		insert(1),
-		text({ "}" }),
-	}),
-
-	s("func", {
-		text({ "func " }),
-		insert(1, "Name"),
-		text({ "(" }),
-		insert(2),
-		text({ ") " }),
-		insert(3),
-		text({ " {", "}" }),
-	}),
-
-	s("method", {
-		text({ "func (" }),
-		insert(1, "Type"),
-		text({ ") " }),
-		insert(2, "Name"),
-		text({ "(" }),
-		insert(3),
-		text({ ") " }),
-		insert(4),
-		text({ " {", "}" }),
+		text({ " uint", "", "" }),
+		text({ "const (", ")" }),
 	}),
 })
